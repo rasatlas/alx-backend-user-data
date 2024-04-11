99dbb86dc7b89b8019ff988458a32f7e1fe68fca
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-12 02:43:38 +0300

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,14 @@ import re
 from typing import List
 
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """A function that returns the log message obfuscated."""
-    return re.sub(
-        r"({}=)([^{}]+)".format("|".join(fields), separator), r"\1" +
-        redaction, message
-    )
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
