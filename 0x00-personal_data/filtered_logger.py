6f9ef0b49105d33307af0ad87c2ebb0c45c160fb
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-12 02:32:36 +0300

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """A function that returns the log message obfuscated."""
     return re.sub(
