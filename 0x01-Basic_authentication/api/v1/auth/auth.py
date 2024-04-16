8bca451b524ca8d0d3f02bee6c66d9a559c676d0
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-16 17:24:07 +0300

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,13 @@ class Auth:
     """Template for all authentication system."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require_auth method."""
+        """Method to check if authentication is required."""
         return False
 
     def authorization_header(self, request=None) -> str:
-        """authorization_header method."""
+        """Method to get the authorization header."""
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
-        """current_user method."""
+        """Method to get the current user."""
         return None
