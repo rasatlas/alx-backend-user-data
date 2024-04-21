901d8865cb32b40d4209998e1901e8f120a02573
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-21 16:12:33 +0300

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,6 +51,7 @@ def authenticate_user():
             "/api/v1/status/",
             "/api/v1/unauthorized/",
             "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
@@ -60,6 +61,11 @@ def authenticate_user():
                 abort(401)
             if request.current_user is None:
                 abort(403)
+            if (
+                auth.authorization_header(request)
+                and auth.session_cookie(request)
+            ):
+                return None, abort(401)
 
 
 if __name__ == "__main__":
