24781cdf3d5ac793f4953727c24faa8d711a5834
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-21 14:43:36 +0300

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,10 +51,11 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
+            # Assign current_user to request
+            request.current_user = auth.current_user(request)
             if auth_header is None:
                 abort(401)
-            if user is None:
+            if request.current_user is None:
                 abort(403)
 
 
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -18,7 +18,7 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
+    """ GET /api/v1/users/:id or GET /api/v1/users/me
     Path parameter:
       - User ID
     Return:
@@ -27,6 +27,11 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        # Return authenticated user
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
