c8c5c3fa6bc3e2d70d1e92cd4b9df0357b834ec1
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-23 18:14:17 +0300

    Create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Method that save the user to the database.
         Args:
             email(str): email of the user
@@ -38,7 +38,7 @@ class DB:
         Return:
             User object
         """
-        user = User(email=email, hashed_password=password)
+        user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
         session.commit()
