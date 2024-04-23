09d94e8a46826dbc1699c473f28b2de1f7770c0b
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-23 15:36:26 +0300

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,16 +2,16 @@
 """User model"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from typing import Optional
 
 Base = declarative_base()
 
 
 class User(Base):
     """User model definition"""
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    __tablename__: str = 'users'
+    id: Column = Column(Integer, primary_key=True)
+    email: Column = Column(String, nullable=False)
+    hashed_password: Column = Column(String, nullable=False)
+    session_id: Optional[Column] = Column(String, nullable=True)
+    reset_token: Optional[Column] = Column(String, nullable=True)
