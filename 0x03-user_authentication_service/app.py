f0fd9606a4e0701ef71f8eebf39b98b872207c0c
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-24 18:18:37 +0300

    7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
 """A basic Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """End-point to register a user.
+    - The end-point should expect two form data fields:
+        - email
+        - password.
+    - If the user does not exist, the end-point should register it and
+    respond with a JSON payload.
+    - If the user is already registered, catch the exception and return
+    JSON payload of the form.
+    """
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    if email and password:
+        try:
+            AUTH.register_user(email, password)
+            return jsonify({"email": "{email}", "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
