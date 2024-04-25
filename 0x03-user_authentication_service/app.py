5d66af2393d0977f6977b11c44d37ffe7fc0f578
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-25 16:30:12 +0300

    14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,12 +58,14 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Function to respond to the DELETE /sessions route."""
-    session_id = request.cookies.get('session_id', None)
+    session_id = request.cookies.get('session_id')
     try:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
             return redirect('/')
+        else:
+            abort(403)
     except Exception:
         abort(403)
 
