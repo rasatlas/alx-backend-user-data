09e668bbc1b7bbf517997e0c940ea5e10b720dbf
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-25 17:53:08 +0300

    16. Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -115,9 +115,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            if not user:
-                raise ValueError
-            else:
+            if user:
                 reset_token = _generate_uuid()
                 self._db.update_user(user.id, reset_token=reset_token)
                 return reset_token
