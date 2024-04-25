997648ecaebe92db892b496d35b97934db6a50bb
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-25 15:47:20 +0300

    13. Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,8 +98,7 @@ class Auth:
             - None
         """
         try:
-            user = self._db.find_user_by(user_id=user_id)
-            self._db.update_user(user.user_id, session_id=None)
+            self._db.update_user(user_id, session_id=None)
             return None
         except Exception:
             return None
